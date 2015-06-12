fa3458c221cbf4e006e24f59877870c5792b46f0
Mark Sliva <mark.sliva@vibes.com>
2015-06-12 13:01:26 -0500

    set request type

--- main.js
+++ main.js
@@ -38,11 +38,13 @@ function sendSubscriptionToServer(subscription) {
   $.ajax({
     url: "https://public-api-uat.vibescm.com/mobile_apps/3b7a9d41-32a9-4277-af2f-94435136afcf/register",
     data: {
-      id: subscription.subscriptionId,
-      registration_id: subscription.subscriptionId,
-      os: 'chrome'
+      device: {
+        id: subscription.subscriptionId,
+        registration_id: subscription.subscriptionId,
+        os: 'chrome'
+      }
     },
-    method: 'POST',
+    type: 'POST',
     contentType: 'application/json',
     complete: function(response) {
       console.log(response);
